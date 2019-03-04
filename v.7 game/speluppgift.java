aa5dd533109ae6144c0174edc25e469a515cabb3
icetea2001 <isac.thoresson.school@gmail.com>
2019-03-04 10:23:49 +0100

    Update speluppgift.java

--- v.7 game/speluppgift.java
+++ v.7 game/speluppgift.java
@@ -12,8 +12,10 @@ public class speluppgift {
 		int numberoftries = 0;
 		int guess;
 		boolean win = false;
+		boolean exit = false;
 		System.out.println("guess a number between 1 and 1000: ");
 	
+		do {
 		while (win == false) {            //gör while loop (för vet inte hur många gånger den kommer gå om) för att man ska kunna testa tills man har rätt
 		
 		
@@ -41,6 +43,19 @@ public class speluppgift {
 		
 		System.out.println("you win!");
 		System.out.println("it took you " + numberoftries + " times");
+		
+		System.out.println("Villu köra igen? tryck 1");
+		guess = input.nextInt();
+		if (guess == 1) {
+			win = false;
+			numberoftries = 0;
+			numberguess = rand.nextInt(1000); 
+			System.out.println("guess a number between 1 and 1000: ");
+		}else {
+			exit = true;
+		}
+		
+		}while(!exit);
 		
 	}
 
