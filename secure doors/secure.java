9edfb3b5c52203067b43bc96c141a4e1115767e6
icetea2001 <isac.thoresson.school@gmail.com>
2019-03-25 10:08:42 +0100

    kommentarer
    uppdaterar lite kommentarer

--- secure doors/secure.java
+++ secure doors/secure.java
@@ -1,15 +1,15 @@
 import java.util.Scanner;
 import java.util.ArrayList;
 public class secure{
-	static ArrayList<String> nameList = new ArrayList<>();
+	static ArrayList<String> nList = new ArrayList<>();
 	static Scanner input = new Scanner(System.in);
 	
-	static boolean output = false;
+	static boolean output;
     static int xcount = 0;
     static int co1 = 0;
     
     public static void main(String[] args) { start();
-    
+    output = false;
     input.nextLine(); //buffer
         inputCheck(); }
 
@@ -21,16 +21,16 @@ public class secure{
     public static void inputCheck() {
         while(!output) {
             String[] nameInput = input.nextLine().split(" ");
-            if (nameInput[0].equals("entry") && nameList.contains(nameInput[1])){
+            if (nameInput[0].equals("entry") && nList.contains(nameInput[1])){
                 System.out.println(nameInput[1] + " entered ANOMALY"); co1++;}
-            if(nameInput[0].equals("out") && !nameList.contains(nameInput[1])) {
-                System.out.println(nameInput[1] + " outed ANOMALY"); co1++;}
+            if(nameInput[0].equals("exit") && !nList.contains(nameInput[1])) {
+                System.out.println(nameInput[1] + " exited ANOMALY"); co1++;}
             if(nameInput[0].equals("entry")) {
-                nameList.add(nameInput[1]);
+                nList.add(nameInput[1]);
                 System.out.println(nameInput[1] + " entered"); co1++;}
-            if(nameInput[0].equals("out")) {
-                nameList.remove(nameInput[1]);
-                System.out.println(nameInput[1] + " outed"); co1++; } 
+            if(nameInput[0].equals("exit")) {
+                nList.remove(nameInput[1]);
+                System.out.println(nameInput[1] + " exited"); co1++; } 
             if(co1 == xcount) {
                 output = true;
             }
